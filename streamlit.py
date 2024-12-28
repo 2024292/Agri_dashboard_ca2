dd0482775936c1a09604353ad9c814e423b6611d
2024292 <2024292@student.cct.ie>
2024-12-28 17:21:13 +0000

    read

--- streamlit.py
+++ streamlit.py
@@ -2,4 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Test Streamlit')
+pd.read_csv('Datasets/Ireland_dairy_list.csv')
+pd.read_csv('Datasets/Four_dairy_ex.csv')
+st.write('Hello world')
